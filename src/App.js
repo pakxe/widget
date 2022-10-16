5eea1332c985f8f7981ca3ec1ff281bffd6bafce
pakxe <pigkill40@naver.com>
2022-10-17 00:34:15 +0900

    chore: styled-component 예제1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ const Back = styled.div`
   width: 100%;
   background-color: pink;
 `;
+const Title = styled.h1`
+  // Title 컴포넌트 = h1태그를 스타일링한 컴포넌트
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  // Wrapper 컴포넌트 = section태그를 스타일링한 컴포넌트
+  padding: 4em;
+  background: papayawhip;
+`;
 
 function App() {
-  return <Back>hi</Back>;
+  return (
+    <>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Flex/index.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.direction || "row"};
+  justify-content: ${(props) => props.justify || "initial"};
+  align-items: ${(props) => props.align || "initial"};
+`;
