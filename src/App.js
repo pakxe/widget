2ce9db8d32f3e6f9993d7063eedcbf7c546da0ff
pakxe <pigkill40@naver.com>
2022-10-17 00:47:51 +0900

    chore: 예제3 컴포넌트 상속

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  background: ${(props) => (props.on ? "palevioletred" : "white")};
-  color: ${(props) => (props.on ? "white" : "palevioletred")};
-
+  // 함수를 CSS 속성에 전달해줬다. 삼항연산자로 어떤 색을 적용할지 선택한다.
+  background: white;
+  color: palevioletred;
   border: 2px solid palevioletred;
   border-radius: 3px;
   padding: 10px;
   margin: 10px;
 `;
 
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+
 function App() {
   return (
     <>
-      <Button>OFF</Button>
-      <Button on>ON</Button>
+      <Button>Normal Button</Button>
+      <TomatoButton>Tomato Button</TomatoButton>
     </>
   );
 }
