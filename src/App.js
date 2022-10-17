6da561cd1c1bd10ee7bc4a09d212e6e54b30ef14
pakxe <pigkill40@naver.com>
2022-10-17 18:10:45 +0900

    chore: selector 연습

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import styled from "styled-components";
 
-const Button = styled.button`
-  // color props가 있다면 그 props의 값으로 배경색을 지정한다.
-  // 전달받은 props가 없다면 기본값인 palevioletred로 배경색을 지정한다.
-  background: ${(props) => props.color || "palevioletred"};
-  color: white;
+const Thing = styled.div`
+  color: blue;
 
-  border: 2px solid white;
-  padding: 10px;
-  margin: 10px;
+  &:hover {
+    // <Thing>에 hover상태일 때
+    color: red;
+  }
+
+  & ~ & {
+    // 첫번째 출현하는 <Thing> 뒤에 있는 모든 <Thing>에게 적용. 첫번째 <Thing>은 적용되지 않음
+    background: tomato;
+  }
+
+  & + & {
+    // <Thing> 출현 후 바로 뒤따르는 B요소에게 적용
+    background: lime;
+  }
+
+  &.something {
+    // 클래스명이 'something'인 <Thing>에게 적용
+    background: orange;
+  }
+
+  .something-else & {
+    // 클래스명이 'something-else'인 임의의 컴포넌트 안에 있는 모든 <Thing>
+    border: 1px solid red;
+  }
 `;
 
 function App() {
   return (
     <>
-      <Button color="tomato">OFF</Button>
-      <Button>ON</Button>
+      <Thing>normal</Thing>
+      <Thing>& + &</Thing>
+      <Thing className="something">
+        Im Thing, and my className='something'
+      </Thing>
+      <div>im div tag</div>
+      <Thing>& ~ &</Thing>
+      <div className="something-else">
+        <Thing>
+          Im Thing inside div tag, and the div className='something'
+        </Thing>
+      </div>
     </>
   );
 }
