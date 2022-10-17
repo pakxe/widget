3694b74a7503047ed444ba67fb0307f61e6a6eaf
pakxe <pigkill40@naver.com>
2022-10-17 22:22:20 +0900

    chore: attrs 생성자 함수 예제

--- src/App.js
+++ src/App.js
@@ -1,62 +1,26 @@
 import styled from "styled-components";
 import { css } from "styled-components";
 
-// Input, LabelText를 감쌀 Label
-const Label = styled.label`
-  align-items: center;
-  display: flex;
-  gap: 8px;
-  margin-bottom: 8px;
-`;
+const Input = styled.input.attrs((props) => ({
+  // 미리 props를 설정한다. Input 컴포넌트는 <input type='text' size='1em'>와 같아진다.
+  type: "text",
+  size: props.size || "1em",
+}))`
+  // css 스타일링
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
 
-// checkbox인 input
-const Input = styled.input.attrs({ type: "checkbox" })``;
-const LabelText = styled.span`
-  // 아래처럼 앞부분에 작성하면 props를 많이 사용해야하는 코드에서 편리하다. props반복작성할 필요가 없으므로..
-  ${(props) => {
-    switch (props.$mode) {
-      case "dark": // props.$mode의 값이 'dark'인 경우
-        return css`
-          background-color: black;
-          color: white;
-          ${Input}: checked + && {
-            // 체크된 Input 컴포넌트 다음에 오는 <LabelText>에 적용
-            color: blue;
-          }
-        `;
-      default: // 그 외
-        return css`
-          background-color: white;
-          color: black;
-          ${Input}: checked + && {
-            color: red;
-          }
-        `;
-    }
-  }}
+  margin: ${(props) => props.size};
+  padding: ${(props) => props.size};
 `;
 
 function App() {
   return (
     <>
-      <Label>
-        <Input defaultChecked />
-        <LabelText>Foo</LabelText>
-      </Label>
-      <Label>
-        <Input />
-        <LabelText $mode="dark">Foo</LabelText>
-        <LabelText $mode="dark">Foo</LabelText>
-      </Label>
-      <Label>
-        <Input defaultChecked />
-        <LabelText $mode="dark">Foo</LabelText>
-        <LabelText $mode="dark">Foo</LabelText>
-      </Label>
-      <Label>
-        <Input defaultChecked />
-        <LabelText>Foo</LabelText>
-      </Label>
+      <Input></Input>
+      <Input size="2em"></Input>
     </>
   );
 }
